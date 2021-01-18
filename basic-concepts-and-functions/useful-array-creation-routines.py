436a6acddaddd21c9256e29ea0e7af368934e718
jeantardelli <jeantardelli@gmail.com>
2021-01-18 21:31:25 +0100

    Basic concept and functions - Numpy Higher dimensional arrays and useful routines (1c)

---
+++ basic-concepts-and-functions/useful-array-creation-routines.py
@@ -0,0 +1,8 @@
+"""
+This module generates numbers at regular interval between two given end points
+using arange and linspace.
+"""
+import numpy as np
+
+np.linspace(0, 1, 5) # array([0., 0.25, 0.5, 0.75, 1.])
+np.arange(0, 1, 0.3) # array([0.0, 0.3, 0.6, 0.9])
