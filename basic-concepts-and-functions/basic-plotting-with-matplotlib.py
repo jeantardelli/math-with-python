99eeff40219b077559015c586c94b867e980b44e
jeantardelli <jeantardelli@gmail.com>
2021-01-22 18:27:59 +0100

    Math plotting w/ matplotlib - basic plotting (2a)

---
+++ basic-concepts-and-functions/basic-plotting-with-matplotlib.py
@@ -0,0 +1,14 @@
+"""
+This module illustrates how to create a basic plot using matplotlib library.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return x*(x-2)*np.exp(3-x)
+
+x = np.linspace(-0.5, 3.0, 100) # 100 values between -0.5 and 3.0
+y = f(x)
+
+ax = plt.plot(x, y)
+plt.show()
